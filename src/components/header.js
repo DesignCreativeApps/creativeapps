fb698d41a42f10a91eec6f704cc943fec7cc51e2
lucacimo <luca.cimonetti@gmail.com>
2019-12-23 16:08:09 +0100

    added check on window object

--- src/components/header.js
+++ src/components/header.js
@@ -28,7 +28,7 @@ class Header extends React.Component {
 
 	render() {
 		let headerClasses;
-		const path = typeof window !== 'undefined' ? window.location.pathname : 'Header HeaderNormal';
+		const path = typeof window !== 'undefined' ? window.location.pathname : '/';
 		if (path != "/") {
 			headerClasses = "Header HeaderNormal"
 		}
