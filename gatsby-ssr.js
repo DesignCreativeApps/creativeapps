97be304c1f9e5f2ab2cb67eead8fa8c9fa1c5774
lucacimo <luca.cimonetti@gmail.com>
2020-01-15 15:39:10 +0100

    fixed scrolling

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+import { globalHistory } from "@reach/router";
+exports.onRenderBody = (
+  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+  pluginOptions
+) => {
+    console.log(globalHistory.location.pathname);
+}
