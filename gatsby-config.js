a6e3e548466654acbbfa0605968566f2c199bdbe
lucacimo <luca.cimonetti@gmail.com>
2020-01-26 13:12:50 +0100

    reverted font

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: true,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
